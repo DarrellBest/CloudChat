fcada115bf515a5afa8e7072a2527d58a55cc9a9
BuildTools <unconfigured@null.spigotmc.org>
2017-02-08 11:29:56 -0500

    Added a new package for classes that commands that do not put players into singleton class arraylists but channels instead. Example team and maybe private messages.

---
+++ src/com/angelcraftonomy/bungeecloudchat/classes/Team.java
@@ -0,0 +1,16 @@
+package com.angelcraftonomy.bungeecloudchat.classes;
+
+import java.util.ArrayList;
+
+import com.angelcraftonomy.bungeecloudchat.CloudChat;
+import com.angelcraftonomy.bungeecloudchat.CloudChatSingleton;
+import com.angelcraftonomy.bungeecloudchat.extenders.CommandExtender;
+import com.angelcraftonomy.bungeecloudchat.interfaces.CommandInterface;
+
+public class Team {
+	private ArrayList<String> players = new ArrayList<String>();
+	private String creator;
+	private String password;
+	private String name;
+	
+}
