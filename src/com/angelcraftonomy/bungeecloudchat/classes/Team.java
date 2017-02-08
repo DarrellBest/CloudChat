0088267d4a0394d4652eb71c265bc2d2b10bece1
BuildTools <unconfigured@null.spigotmc.org>
2017-02-08 12:05:22 -0500

    Updated Team Class added add and remove player and constructor

--- src/com/angelcraftonomy/bungeecloudchat/classes/Team.java
+++ src/com/angelcraftonomy/bungeecloudchat/classes/Team.java
@@ -7,10 +7,34 @@ import com.angelcraftonomy.bungeecloudchat.CloudChatSingleton;
 import com.angelcraftonomy.bungeecloudchat.extenders.CommandExtender;
 import com.angelcraftonomy.bungeecloudchat.interfaces.CommandInterface;
 
+import net.md_5.bungee.api.plugin.Command;
+
 public class Team {
-	private ArrayList<String> players = new ArrayList<String>();
-	private String creator;
-	private String password;
-	private String name;
 	
+	// Array List of players on a team
+	private ArrayList<String> players = new ArrayList<String>();
+	
+	//Team settings
+	private String teamName;
+	private String password;
+	private String creator;
+	
+	
+	public Team(String teamName, String password, String creator) {
+		teamName = this.teamName;
+		if(this.password != null){
+			password = this.password;
+		} else {
+			password = null;
+		}
+		creator = this.creator;
+	}
+	
+	public boolean addPlayer(String name){
+		return true;
+	}
+	
+	public boolean removePlayer(String name){
+		return true;
+	}
 }
