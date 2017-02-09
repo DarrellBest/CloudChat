bac4682f353e1a08d18722f430d6b89370acb893
Darrell Best <dsbest@g.clemson.edu>
2017-02-09 00:25:02 -0500

    moved test command to args 2

--- src/com/angelcraftonomy/bungeecloudchat/CommandExecuter.java
+++ src/com/angelcraftonomy/bungeecloudchat/CommandExecuter.java
@@ -38,13 +38,12 @@ public class CommandExecuter extends Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
+		String commandName = args[0];
 		this.sender = sender;
 
 		if (args.length == 0)
 			toggleGlobal(sender);
 
-		String commandName = args[0];
-		
 		if (args.length == 1) {
 
 			if (commandName.equalsIgnoreCase(socialSpyCommand.getName())
@@ -67,18 +66,11 @@ public class CommandExecuter extends Command {
 				listCommand.run();
 				listCommand.cleanup();
 			}
-			
-			if (commandName.equalsIgnoreCase(testCommand.getName())
-					|| commandName.equalsIgnoreCase(testCommand.getAlias())) {
-				testCommand.initialize(sender, args);
-				testCommand.run();
-				testCommand.cleanup();
-			}
-			
+
 		}
-		
+
 		if (args.length == 2) {
-			
+
 			if (commandName.equalsIgnoreCase(testCommand.getName())
 					|| commandName.equalsIgnoreCase(testCommand.getAlias())) {
 				testCommand.initialize(sender, args);
@@ -86,7 +78,7 @@ public class CommandExecuter extends Command {
 				testCommand.cleanup();
 			}
 		}
-		
+
 	}
 
 	// Might need to make a new command for this, but this way players can still
